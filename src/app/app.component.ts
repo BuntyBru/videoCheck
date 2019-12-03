12b99ce33107874d72a5f32237155cdff1037a9f
Buntybru <buntybru@gmail.com>
2019-12-03 18:07:01 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { EmbedVideoService } from 'ngx-embed-video';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  points: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Robert', points: 1.0079},
+  {position: 2, name: 'De', points: 4.0026},
+  {position: 3, name: 'Niro', points: 6.941}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'tables';
+  youtubeUrl:string='https://www.youtube.com/watch?v=Run4JZs4Qc8';
+  youtubeId = 'Run4JZs4Qc8';
+  displayedColumns: string[] = ['position', 'name', 'points'];
+
+  dataSource = ELEMENT_DATA;
+  constructor(private embedService: EmbedVideoService)
+  {
+    console.log(this.embedService.embed_youtube(this.youtubeId));
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { EmbedVideo } from 'ngx-embed-video';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidePanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,HttpClientModule, EmbedVideo.forRoot(),
+    MatTableModule,MatCardModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
